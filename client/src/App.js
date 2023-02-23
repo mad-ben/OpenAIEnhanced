65c705d2cb3f57f35aba7d79222c78927e0b3f35
Andriy Kalysh <53571019+mad-ben@users.noreply.github.com>
2023-02-23 23:03:33 +0000

    0.9 Done
    Server receives all params. UI finished.
    
    TODO Make APP Responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,95 +1,157 @@
-import './App.css';
-import './normal.css';
-import { useState, useEffect } from 'react';
-import { ChatMessageComp } from './components';
+import "./App.css";
+import "./normal.css";
+import { useState, useEffect } from "react";
+import { ChatMessageComp } from "./components";
 
 function App() {
-
   //use effect as soon as app runs
   useEffect(() => {
     getEngines();
-  }, [])
+  }, []);
 
   const [input, setInput] = useState("");
   const [models, setModels] = useState([]);
   const [currentModel, setCurrentModel] = useState("text-davinci-003");
+  const [responceLeng, setResponceLeng] = useState("100");
+  const [temp, setTemp] = useState("0");
   const [chatLog, setChatLog] = useState([]);
 
   //Clear chats
 
-function clearChat(){
-  setChatLog([]);
-}
+  function clearChat() {
+    setChatLog([]);
+  }
 
-function getEngines(){
-  fetch("http://localhost:3080/models")
-  .then(res => res.json())
-  .then(data => {
-    console.log(data.models)
-    setModels(data.models)
-  })
-}
+  function resetDefaults() {
+    setCurrentModel("text-davinci-003");
+    setResponceLeng("100");
+    setTemp("0.5");
+  }
 
-  async function handleSubmit(e){
+  function getEngines() {
+    fetch("http://localhost:3080/models")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.models);
+        setModels(data.models);
+      });
+  }
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    let chatLogNew = [...chatLog, {user: "me", message: `${input}`}]
+    let chatLogNew = [...chatLog, { user: "me", message: `${input}` }];
     setInput("");
-    setChatLog(chatLogNew)
+    setChatLog(chatLogNew);
     //console.log("submit")
 
-    const messages = chatLogNew.map((message) => message.message).join("\n")
-    const response = await fetch ("http://localhost:3080/", {
+    const messages = chatLogNew.map((message) => message.message).join("\n");
+    const response = await fetch("http://localhost:3080/", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         message: messages,
         currentModel,
-      })
+        responceLeng,
+        temp,
+      }),
     });
 
     const data = await response.json();
-    await setChatLog([...chatLogNew, {user: "gpt", message: `${data.message}`}])
+    await setChatLog([
+      ...chatLogNew,
+      { user: "gpt", message: `${data.message}` },
+    ]);
     console.log(data.message);
   }
 
   return (
     <div className="App">
       <aside className="sidemenu">
-
-        <div className="side-menu-button" onClick={clearChat}>
-          <span>+</span>
-          New Chat
+        <h2>JERICHO Enhanced Chat</h2>
+        <hr class="rounded"></hr>
+        <div>
+          <div className="side-menu-button" onClick={clearChat}>
+            New Chat
+          </div>
+          <div className="side-menu-button" onClick={resetDefaults}>
+            Reset Values
+          </div>
+          <a href="https://jericho-project.vercel.app/">
+            <div className="side-menu-button">Home</div>
+          </a>
         </div>
-        
-       
-        <p className="advanced-settings">
-          Advanced Settings
-        </p>
+        <div className="advanced-menu">
+          <hr class="rounded"></hr>
 
-        <div className="models">
-          <p className="model-text">
-            OpenAI Model List
-          </p>
-          <select onChange={(e) => {
-              setCurrentModel(e.target.value)
-            }} className="model-select">
-              {models.map((model,index) => (
-                <option key={model.id} value={model.id}>{model.id}</option>
+          <p className="advanced-settings">Advanced Settings</p>
+
+          <div>
+            <p className="model-text">OpenAI Model List</p>
+            <p className="instruction-text">
+              The OpenAI API is powered by a family of models with different
+              capabilities.
+            </p>
+            <select
+              onChange={(e) => {
+                setCurrentModel(e.target.value);
+              }}
+              className="model-select"
+            >
+              {models.map((model, index) => (
+                <option key={model.id} value={model.id}>
+                  {model.id}
+                </option>
               ))}
-          </select>
-        </div>      
+            </select>
+          </div>
+
+          <div>
+            <p className="model-text">Response length</p>
+
+            <input
+              type="text"
+              placeholder="100"
+              className="number-select"
+              onChange={(e) => {
+                setResponceLeng(e.target.value);
+              }}
+            ></input>
+
+            <p className="desc-text">
+              Insert maximum amount of words to display <br /> (Min 1 - Max
+              2000)
+            </p>
+          </div>
+
+          <div>
+            <p className="model-text">Temperature</p>
+            <p className="instruction-text">
+              Randomness of the response. 0 means deterministic and repetitive
+              while 1 is random and creative.
+            </p>
+
+            <input
+              type="text"
+              placeholder="0.5"
+              className="number-select"
+              onChange={(e) => {
+                setTemp(e.target.value);
+              }}
+            ></input>
+            <p className="desc-text">(Min 0 - Max 1)</p>
+            <hr class="rounded"></hr>
+          </div>
+        </div>
       </aside>
 
-
       {/** Division between sidemenu and main chat */}
-
 
       <section className="chatbox">
         <div className="chat-log">
           {chatLog.map((message, index) => (
-            <ChatMessageComp key={index} message={message}/>
+            <ChatMessageComp key={index} message={message} />
           ))}
         </div>
 
@@ -98,12 +160,11 @@ function getEngines(){
             <input
               rows="1"
               value={input}
-              onChange={(e)=> setInput(e.target.value)}
+              onChange={(e) => setInput(e.target.value)}
               className="chat-input-textarea"
               placeholder="Type your question..."
-            >
-            </input>
-          </form>          
+            ></input>
+          </form>
         </div>
       </section>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,45 +1,46 @@
 import { Configuration, OpenAIApi } from "openai";
-import express from 'express';
+import express from "express";
 import bodyParser from "body-parser";
-import cors from 'cors';
-import * as dotenv from 'dotenv'
+import cors from "cors";
+import * as dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const port = 3080
+const port = 3080;
 
-app.post('/', async (req, res) => {
-    const { message, currentModel } = req.body;
-    console.log(message)
-    const response = await openai.createCompletion({
-        model: `${currentModel}`,
-        prompt: `${message}`,
-        max_tokens: 100,
-        temperature: 0.5,
-      });
-      res.json({
-        message: response.data.choices[0].text
-      })
+app.post("/", async (req, res) => {
+  const { message, currentModel, responceLeng, temp } = req.body;
+  console.log(responceLeng);
+  console.log(temp);
+  const response = await openai.createCompletion({
+    model: `${currentModel}`,
+    prompt: `${message}`,
+    max_tokens: parseInt(responceLeng),
+    temperature: parseInt(temp),
+  });
+  res.json({
+    message: response.data.choices[0].text,
+  });
 });
 
-app.get('/models', async (req, res) => {
-    const response = await openai.listModels();
-    console.log(response.data.data)
-    res.json({
-        models: response.data.data
-      })
+app.get("/models", async (req, res) => {
+  const response = await openai.listModels();
+  //console.log(response.data.data);
+  res.json({
+    models: response.data.data,
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
